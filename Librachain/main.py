f93c73fd42497dc43a101c88b31f8c72cb5d0c15
chiara <s1093786@studenti.univpm.it>
2023-03-27 11:20:23 +0200

    onchaincontroller

--- Librachain/main.py
+++ Librachain/main.py
@@ -1,6 +1,12 @@
-from cli.CommandLineInterface import CommandLineInterface
+import controller
+from controller.OnChainController import OnChainController
 
 if __name__ == "__main__":
-    cli = CommandLineInterface()
-    while (True):
-        cli.print_menu()
+
+    #cli = CommandLineInterface()
+    #while (True):
+    #    cli.print_menu()
+    myWallet = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+    invoke_onchain = OnChainController()
+    invoke_onchain.addToDictionary("http://127.0.0.1:8548", "0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B", myWallet)
+    invoke_onchain.getDeployed("http://127.0.0.1:8548")
