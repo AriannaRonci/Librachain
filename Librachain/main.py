bb3fb4ddd474506b852a11ee9ec06b0f91439d74
chiara <s1093786@studenti.univpm.it>
2023-03-28 12:36:45 +0200

    merge

--- Librachain/main.py
+++ Librachain/main.py
@@ -5,17 +5,11 @@ from cli.CommandLineInterface import CommandLineInterface
 from controller.OnChainController import OnChainController
 
 if __name__ == "__main__":
-
+    myWallet = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+    invoke_onchain = OnChainController()
     invoke_onchain.getBalance("http://127.0.0.1:8548")
     invoke_onchain.getDeployed("http://127.0.0.1:8548")
     invoke_onchain.addToDictionary("http://127.0.0.1:8548", "0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B", myWallet)
-    invoke_onchain = OnChainController()
-    myWallet = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
     cli = CommandLineInterface()
     while (True):
         cli.print_menu()
-
-    #myWallet = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
-    #invoke_onchain = OnChainController()
-    #invoke_onchain.addToDictionary("http://127.0.0.1:8548", "0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B", myWallet)
-    #invoke_onchain.getDeployed("http://127.0.0.1:8548")
