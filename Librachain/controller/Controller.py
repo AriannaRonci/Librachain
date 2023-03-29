f3427cd2ea49c6a3a6a3550eb1b37d64b19a9e53
OT-Rax <rahmi.elmechri@gmail.com>
2023-03-29 14:22:17 +0200

    db a little further

--- Librachain/controller/Controller.py
+++ Librachain/controller/Controller.py
@@ -4,23 +4,25 @@ class Controller:
     
     __init__(self, session):
         self.user_repo = UserRepository()
+        self.session = session
 
     def login(self, username, password, public_key, private_key):
-        #Mettiamo tutta la logica dei tentativi qui?
-        #if ...
-        #   session.setUser(user)
-        #   return True
-        #else
-        #    return False
-        #    session.incrementLoginAttempts()
-        pass
+        if session.attempts < 5 and self.user_repo.check_password(username, password):
+            session.setUser(user)
+            return True
+        elif session.attempts < 5:
+            session.incrementLoginAttempts()
+            return False
+        else:
+            return False
 
     def register(self, username, password, public_key, private_key):
-        #self.user_repo.register(username, password, public_key, private_key)
-        #user = self.user_repo.get_user_by_username(username)
-        #session.setUser(user)
-        pass
+        if self.user_repo.register(username, password, public_key, private_key):
+            user = self.user_repo.get_user_by_username(username)
+            session.setUser(user)
+            return True
+        else:
+            return False
 
     def decrypt_private_key(self, encrypted_private_key, password):
         return self.user_repo.decrypt_private_key(encrypted_private_key, password)
-
