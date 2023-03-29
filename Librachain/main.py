16969380d5fce796bdc41a12e7af9bd39831e31c
chiara <s1093786@studenti.univpm.it>
2023-03-29 12:29:35 +0200

    deploy contracts

--- Librachain/main.py
+++ Librachain/main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     myWallet = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
     invoke_onchain = OnChainController()
     invoke_onchain.getBalance("http://127.0.0.1:8548")
-    invoke_onchain.getDeployed("http://127.0.0.1:8548")
+    invoke_onchain.getAddressList("http://127.0.0.1:8548")
     invoke_onchain.addToDictionary("http://127.0.0.1:8548", "0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B", myWallet)
     #cli = CommandLineInterface()
     #while (True):
