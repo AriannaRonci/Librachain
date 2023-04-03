76a8fa1533879773ff3e29b95b52a73c0afa80a4
OT-Rax <rahmi.elmechri@gmail.com>
2023-04-03 10:36:57 +0200

    session

--- Librachain/session/session.py
+++ Librachain/session/session.py
@@ -1,8 +1,8 @@
 #import uuid
 
 class Session:
-    def __init__(self, user):
-        self.user = user
+    def __init__(self):
+        self.user = None
         #self.id = str(uuid.uuid4())
         self.attempts = 0
 
