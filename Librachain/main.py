cf7cf9a88e9ddc8e6858eab2b4ef1506e85c1b69
OT-Rax <rahmi.elmechri@gmail.com>
2023-04-03 10:32:43 +0200

    some things

--- Librachain/main.py
+++ Librachain/main.py
@@ -1,6 +1,7 @@
 from cli.command_line_interface import CommandLineInterface
 from controllers.controller import Controller
 from controllers.on_chain_controller import OnChainController
+from session.session import Session
 
 if __name__ == "__main__":
     myWallet = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
@@ -8,6 +9,7 @@ if __name__ == "__main__":
     invoke_onchain.getBalance("http://127.0.0.1:8548")
     invoke_onchain.getAddressList("http://127.0.0.1:8548")
     invoke_onchain.addToDictionary("http://127.0.0.1:8548", "0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B", myWallet)
-    #cli = CommandLineInterface()
-    #while (True):
-    #    cli.print_menu()
+    session_obj = Session()
+    cli = CommandLineInterface(session_obj)
+    while (True):
+        cli.print_menu()
