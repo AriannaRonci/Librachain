ff897776589d7cbfcea70ae1d305158526bd9876
AriannaRonci <s1093926@studenti.univpm.it>
2023-04-03 10:22:32 +0200

    ho sonno

--- Librachain/deploy_on_chain.py
+++ Librachain/deploy_on_chain.py
@@ -3,7 +3,6 @@ import solcx
 from web3 import Web3, HTTPProvider
 from solcx import compile_standard, compile_source
 solcx.install_solc('0.6.0')
-import json
 
 # Set up the web3 provider
 web3 = Web3(HTTPProvider('http://localhost:8548'))
@@ -14,6 +13,8 @@ account = web3.eth.accounts[0]
 contract_file = 'soliditycontracts/OnChainManager.sol'
 with open(contract_file) as f:
 	on_chain_source_code = f.read()
+#contract_abi = contract_json['abi']
+#contract_bytecode = contract_json['bytecode']
 
 compiled_contract = compile_source(on_chain_source_code, output_values=['abi', 'bin'])
 contract_id, contract_interface = compiled_contract.popitem()
@@ -28,10 +29,8 @@ bytecode=contract_bytecode)
 tx_hash = MyContract.constructor().transact({'from': account})
 receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
 
-logging.basicConfig(filename='soliditycontracts/contract_address.txt',
-					filemode='w',
-					level=logging.DEBUG,
-					format='')
+logging.basicConfig(filename='soliditycontracts/contract_address.txt', level=logging.DEBUG, format='')
 logging.info(receipt['contractAddress'])
 
 print(dict(receipt))
+
--- Librachain/models/user.py
+++ Librachain/models/user.py
@@ -1,19 +1,19 @@
 class User:
-    def __init__(self, id, username, password_hash, public_key):
+    def __init__(self, id, username, password_hash, public_key, private_key):
         self.id = id
         self.__username = username
         self.__password_hash = password_hash
         self.__public_key = public_key
         self.__private_key = private_key
 
-    def getUsername():
+    def getUsername(self):
         return self.__username
 
-    def getPasswordHash():
+    def getPasswordHash(self):
         return self.__password_hash
         
-    def getPublicKey():
+    def getPublicKey(self):
         return self.__public_key
 
-    def getPrivateKey():
+    def getPrivateKey(self):
         return self.__private_key
