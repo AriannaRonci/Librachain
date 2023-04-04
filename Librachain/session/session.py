2bf568aab0e63e29af2510a24752d0dbfb413dc5
AriannaRonci <s1093926@studenti.univpm.it>
2023-04-04 14:09:43 +0200

    gestione attempts

--- Librachain/session/session.py
+++ Librachain/session/session.py
@@ -1,30 +1,31 @@
-#import uuid
+# import uuid
 import time
+
 
 class Session:
     def __init__(self):
         self.__user = None
-        #self.__id = str(uuid.uuid4())
+        # self.__id = str(uuid.uuid4())
         self.__attempts = 0
         self.__exceededTimestamp = 0
 
     def getUser(self):
-        return self.user
+        return self.__user
 
-    def setUser(self,user):
+    def setUser(self, user):
         self.user = user
 
     def getAttempts(self):
-        return self.attempts
+        return self.__attempts
 
     def incrementLoginAttempts(self):
-        self.attempts += 1
+        self.__attempts += 1
 
     def resetAttempts(self):
-        self.attempts = 0
+        self.__attempts = 0
 
     def setExceededAttemptsTimeout(self):
-        self.__exceededTimestamp = time.time() + 300
-        
-    def getTimeLeftForUnlock():
-        return self.__exceededTimestamp-time.time()
+        self.__exceededTimestamp = time.time() + 30
+
+    def getTimeLeftForUnlock(self):
+        return self.__exceededTimestamp - time.time()
