6aecd85013398662a02f42180f529b549e85f180
chiara <s1093786@studenti.univpm.it>
2023-04-04 13:55:21 +0200

    on chain

---
+++ Librachain/controllers/interaction_controller.py
@@ -0,0 +1,7 @@
+class InteractionController:
+
+    def __init__(self):
+        pass
+
+    def byAbi(self, smartContractAddress, abi):
+        pass
--- Librachain/soliditycontracts/OnChainManager.sol
+++ Librachain/soliditycontracts/OnChainManager.sol
@@ -21,4 +21,29 @@ contract OnChainManager {
     function getBalance(string memory shardAddress) public view returns (int){
         return shardsBalance[shardAddress];
     }
+
+    function compareStrings(address a, address b) public pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+
+    function findString(address[] memory array,address _string) internal pure returns (bool){
+    for (uint i = 0; i < array.length; i++) {
+        address stringToFind = array[i];
+        bool exists = compareStrings(stringToFind, _string);
+        if(exists == true) {
+            return true;
+        }
+    }
+        return false;
+    }
+
+    function getShard(address smartContractAddress) public view returns (string memory){
+        string[3] memory shards = ["http://127.0.0.1:8545", "http://127.0.0.1:8546", "http://127.0.0.1:8547"];
+        for(uint i=0; i<2; i++){
+            string memory shard = shards[i];
+            if (findString(shardsMapping[shard], smartContractAddress)==true)
+                return shard;
+        }
+        return "contract not deployed";
+    }
 }
