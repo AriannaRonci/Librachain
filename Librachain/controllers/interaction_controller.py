fad16f7f1569ad861c7039c75f24c2ad0b902fba
chiara <s1093786@studenti.univpm.it>
2023-04-04 14:08:03 +0200

    snakecase

--- Librachain/controllers/interaction_controller.py
+++
@@ -1,7 +0,0 @@
-class InteractionController:
-
-    def __init__(self):
-        pass
-
-    def byAbi(self, smartContractAddress, abi):
-        pass
---
+++ Librachain/controllers/shards_controller.py
@@ -0,0 +1,8 @@
+class ShardsController:
+
+    def __init__(self, smart_contract_address, abi):
+        self.smartContractAddress = smart_contract_address
+        self.abi = abi
+
+    def by_abi(self, smart_contract_address, abi):
+        pass
