47f3c295cf8813068e2f473d74314ec881623d40
chiara <s1093786@studenti.univpm.it>
2023-04-04 15:28:39 +0200

    bugs

--- Librachain/controllers/shards_controller.py
+++ Librachain/controllers/shards_controller.py
@@ -1,6 +1,10 @@
+import solcx
 from web3 import Web3
 
 from controllers.on_chain_controller import OnChainController
+from solcx import compile_source
+
+solcx.install_solc('0.6.0')
 
 
 class ShardsController:
@@ -11,10 +15,16 @@ class ShardsController:
     def deploy_smart_contract(self, smart_contract_path, wallet):
         with open(smart_contract_path, 'r') as file:
             smart_contract_code = file.read()
-        #compiled_contract = compile_source(smart_contract_code, output_values=['abi', 'bin'])
-        #contract_id, contract_interface = compiled_contract.popitem()
-        #abi = contract_interface['abi']
-
+        compiled_contract = compile_source(smart_contract_code, output_values=['abi', 'bin'])
+        contract_id, contract_interface = compiled_contract.popitem()
+        contract_abi = contract_interface['abi']
+        contract_bytecode = contract_interface['bin']
+        MyContract = self.w3.eth.contract(abi=contract_abi,
+                                          bytecode=contract_bytecode)
+        tx_hash = MyContract.constructor().transact({'from': wallet})
+        receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        print(receipt['contractAddress'])
+        print(dict(receipt))
 
     def by_abi(self, smart_contract_address, abi):
         invoke_onchain = OnChainController()
--- Librachain/main.py
+++ Librachain/main.py
@@ -9,6 +9,52 @@ if __name__ == "__main__":
     invoke_onchain.get_address_list("http://127.0.0.1:8548")
     invoke_onchain.add_to_dictionary("http://127.0.0.1:8548", "0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B", my_wallet)
     session_obj = Session()
+
     cli = CommandLineInterface(session_obj)
     while True:
         cli.print_menu()
+
+    # invoke_onchain.get_balance("http://127.0.0.1:8548")
+    # print("add to dictionary")
+    # print(invoke_onchain.add_to_dictionary("http://127.0.0.1:8545", "0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B", my_wallet))
+    print("get address list")
+    print(invoke_onchain.get_address_list("http://127.0.0.1:8545"))
+    print("invoke get shard")
+    print(invoke_onchain.get_shard("0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B"))
+    shards_controller = ShardsController(Web3(Web3.HTTPProvider("http://127.0.0.1:8545")))
+    print("shards controller abi")
+    print(shards_controller.by_abi("0xCfEB869F69431e42cdB54A4F4f105C19C080A601", [
+        {
+            "inputs": [],
+            "name": "retrieve",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "num",
+                    "type": "uint256"
+                }
+            ],
+            "name": "store",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ]))
+    # session_obj = Session()
+    # cli = CommandLineInterface(session_obj)
+    # while True:
+    #    cli.print_menu()
+
+
+
