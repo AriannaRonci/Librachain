1ccdef31d1b444aa4d75fa79fa4e865385a9a153
OT-Rax <rahmi.elmechri@gmail.com>
2023-04-05 09:33:08 +0200

    ma anche la tua si muove?

--- Librachain/db/db_migration.py
+++ Librachain/db/db_migration.py
@@ -4,12 +4,16 @@ if __name__ == '__main__':
     con=sqlite3.connect('librachain')
     cur=con.cursor()
     cur.execute('''
-        CREATE TABLE Utenti(
+        CREATE TABLE Users(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT NOT NULL UNIQUE,
         password_hash TEXT NOT NULL,
         public_key TEST NOT NULL,
         private_key TEST NOT NULL
+        );
+
+        CREATE TABLE Timestamps(
+            
         );
     ''')
     con.commit()
