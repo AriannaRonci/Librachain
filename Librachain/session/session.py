5add20394755e1714ca5ec647ac72b2c4048dd9b
OT-Rax <rahmi.elmechri@gmail.com>
2023-04-05 16:11:52 +0200

    documented session

--- Librachain/session/session.py
+++ Librachain/session/session.py
@@ -1,7 +1,15 @@
 # import uuid
 import time
+from models.user import User
 
 class Session:
+    """Used to share information on the current session between objects.
+    Attributes:
+        __user: logged in User object
+        __attempts: Integer, current consecutive failed login attempts
+        __exceededTimestamp: Integer, timestamp for login unlock
+    """
+
     def __init__(self):
         self.__user = None
         # self.__id = str(uuid.uuid4())
@@ -9,12 +17,19 @@ class Session:
         self.__exceededTimestamp = 0
 
     def getUser(self):
+        """ __user getter.
+        
+        Returns:
+         A User object or None if no user is logged in.
+        """
         return self.__user
 
-    def setUser(self, user):
+    def setUser(self, user: User):
+        """__user setter"""
         self.__user = user
 
     def getAttempts(self):
+        """__attempts getter"""
         return self.__attempts
 
     def incrementLoginAttempts(self):
@@ -23,8 +38,10 @@ class Session:
     def resetAttempts(self):
         self.__attempts = 0
 
-    def setExceededAttemptsTimeout(self, lock_time):
+    def setExceededAttemptsTimeout(self, lock_time: int):
+        """Sets __exceededTimestamp to lock_time seconds from when called"""
         self.__exceededTimestamp = time.time() + lock_time
 
     def getTimeLeftForUnlock(self):
+        """Returns seconds left till __exceededTimestmap"""
         return self.__exceededTimestamp - time.time()
