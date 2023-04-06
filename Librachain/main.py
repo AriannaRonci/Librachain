6e8d1c75a74fc053d07b1df6452a405401bfa329
chiara <s1093786@studenti.univpm.it>
2023-04-06 18:13:29 +0200

    main

--- Librachain/main.py
+++ Librachain/main.py
@@ -1,9 +1,11 @@
+from eth_utils import decode_hex
 from web3 import Web3
 
 from cli.command_line_interface import CommandLineInterface
 from controllers.on_chain_controller import OnChainController
 from controllers.shards_controller import ShardsController
 from session.session import Session
+from eth_keys import keys
 
 if __name__ == "__main__":
 
@@ -13,6 +15,13 @@ if __name__ == "__main__":
     while True:
         cli.print_menu()
 
+"""    #pk = keys.PrivateKey()
+    x = input('Chiave Privata')
+    priv_key_bytes = decode_hex(x)
+    priv_key = keys.PrivateKey(priv_key_bytes)
+    pk = priv_key.public_key.to_checksum_address()
+    print(pk)
+"""
 
 
 
