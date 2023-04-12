f32dc89399606e1f62a84c9a6860b9d0076536de
AriannaRonci <s1093926@studenti.univpm.it>
2023-04-12 12:25:19 +0200

    ritocchini vari

--- Librachain/cli/command_line_interface.py
+++ Librachain/cli/command_line_interface.py
@@ -129,13 +129,14 @@ class CommandLineInterface:
             if res == 0:
                 print('\nYou are logged in.\n')
                 if self.controller.check_password(username, password):
-                    res= self.suggest_change_password(username)
-                    if res == 0:
-                        print('Password succesfully changed.\n')
-                        self.print_user_options()
-                    elif res == -1:
-                        print('Password not changed.\n')
-                        self.print_user_options()
+                    if self.controller.check_password_obsolete(username):
+                        res= self.suggest_change_password(username)
+                        if res == 0:
+                            print('Password succesfully changed.\n')
+                            self.print_user_options()
+                        elif res == -1:
+                            print('Password not changed.\n')
+                            self.print_user_options()
 
             elif res == -1:
                 print('\nIncorrect username or password\n')
